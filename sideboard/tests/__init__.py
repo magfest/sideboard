884ec580cda79ef2203557c6e728dff2e9b3dece
Rob Ruana <rob@robruana.com>
2017-03-09 13:10:55 -0800

    Fixes #59: Test server uses an ephemeral port instead of binding to 8282 (#65)

--- sideboard/tests/__init__.py
+++ sideboard/tests/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 import os
+import socket
+from contextlib import closing
 
 import pytest
 import sqlalchemy
@@ -7,6 +9,30 @@ from sqlalchemy import event
 from sqlalchemy.orm import sessionmaker
 
 from sideboard.lib import config, services
+
+
+def get_available_port():
+    """
+    Returns an unused port in the ephemeral port range.
+
+    Binding to port 0 with socket.SO_REUSEADDR will give us an unused port
+    that we can immediately reuse. This is mostly safe, but on heavily used
+    systems there is a potential race condition if another process uses
+    the same port in the time between requesting an available port and
+    actually using it. This is unlikely, and the worst that will happen is
+    the tests fail on that particular test run.
+
+    See https://eklitzke.org/binding-on-port-zero
+
+    Ideally we could tell cherrypy to listen on port 0, and then inspect
+    it to determine what port it's using, but cherrypy doesn't support that
+    yet (other parts of cherrypy will try to use the port defined in
+    'cherrypy.server.socket_port' and end up failing on startup).
+    """
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('127.0.0.1', 0))
+        return sock.getsockname()[1]
 
 
 @pytest.fixture
