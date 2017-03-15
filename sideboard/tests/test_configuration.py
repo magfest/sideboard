6bc49e7c9e9aeb2f952b9685a4b65d9d878907d2
Rob Ruana <rob@robruana.com>
2017-03-14 21:08:25 -0700

    Fixes #66: Adds support for SIDEBOARD_CONFIG_OVERRIDES env var

--- sideboard/tests/test_configuration.py
+++ sideboard/tests/test_configuration.py
@@ -4,7 +4,23 @@ import os
 import pytest
 from mock import Mock
 
-from sideboard.config import parse_config, get_config_root
+from sideboard.config import get_config_files, get_config_overrides, \
+    get_config_root, get_module_and_root_dirs, parse_config, uniquify
+
+
+def test_uniquify():
+    assert [] == uniquify([])
+    assert ['a', 'b', 'c'] == uniquify(['a', 'b', 'c'])
+    assert ['a', 'b', 'c', 'd', 'e'] == uniquify(['a', 'b', 'a', 'c', 'a', 'd', 'a', 'e'])
+    assert ['a'] == uniquify(['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'])
+
+
+@pytest.mark.skipif(
+    'SIDEBOARD_CONFIG_OVERRIDES' not in os.environ,
+    reason='SIDEBOARD_CONFIG_OVERRIDES not set')
+def test_test_defaults_ini():
+    from sideboard.lib import config
+    assert config.get('is_test_running')
 
 
 class SideboardConfigurationTest(object):
@@ -33,7 +49,114 @@ class SideboardConfigurationTest(object):
             assert test_config.get('root') == expected_root
 
 
-class TestSideboardConfigRoot(object):
+class TestSideboardGetConfigFiles(object):
+    @pytest.fixture
+    def config_overrides_unset(self, monkeypatch):
+        monkeypatch.delenv('SIDEBOARD_CONFIG_OVERRIDES', raising=False)
+
+    @pytest.fixture
+    def config_overrides_set(self, monkeypatch):
+        monkeypatch.setenv('SIDEBOARD_CONFIG_OVERRIDES', 'test-defaults.ini')
+
+    @pytest.fixture
+    def plugin_dirs(self):
+        module_path = '/opt/sideboard/plugins/test_plugin/test_plugin'
+        root_path = os.path.join(os.getcwd(), 'plugins', 'test_plugin')
+        return (module_path, root_path)
+
+    @pytest.fixture
+    def sideboard_dirs(self):
+        module_path = '/opt/sideboard/sideboard'
+        root_path = '/opt/sideboard'
+        return (module_path, root_path)
+
+    def test_get_module_and_root_dirs_plugin(self, plugin_dirs):
+        assert plugin_dirs == get_module_and_root_dirs(
+            os.path.join(plugin_dirs[0], 'config.py'), is_plugin=True)
+
+    def test_get_module_and_root_dirs_sideboard(self, sideboard_dirs):
+        assert sideboard_dirs == get_module_and_root_dirs(
+            os.path.join(sideboard_dirs[0], 'config.py'), is_plugin=False)
+
+    def test_get_config_files_plugin(
+        self, plugin_dirs, config_overrides_unset):
+
+        expected = [
+            '/etc/sideboard/plugins.d/test-plugin.cfg',
+            os.path.join(plugin_dirs[1], 'development-defaults.ini'),
+            os.path.join(plugin_dirs[1], 'development.ini')]
+        assert expected == get_config_files(
+            os.path.join(plugin_dirs[0], 'config.py'), is_plugin=True)
+
+    def test_get_config_files_sideboard(
+        self, sideboard_dirs, config_overrides_unset):
+
+        expected = [
+            '/etc/sideboard/sideboard-core.cfg',
+            '/etc/sideboard/sideboard-server.cfg',
+            os.path.join(sideboard_dirs[1], 'development-defaults.ini'),
+            os.path.join(sideboard_dirs[1], 'development.ini')]
+        assert expected == get_config_files(
+            os.path.join(sideboard_dirs[0], 'config.py'), is_plugin=False)
+
+    def test_get_config_files_plugin_with_overrides(
+        self, plugin_dirs, config_overrides_set):
+
+        expected = [
+            '/etc/sideboard/plugins.d/test-plugin.cfg',
+            os.path.join(plugin_dirs[1], 'test-defaults.ini'),
+            os.path.join(plugin_dirs[1], 'test.ini')]
+        assert expected == get_config_files(
+            os.path.join(plugin_dirs[0], 'config.py'), is_plugin=True)
+
+    def test_get_config_files_sideboard_with_overrides(
+        self, sideboard_dirs, config_overrides_set):
+
+        expected = [
+            '/etc/sideboard/sideboard-core.cfg',
+            '/etc/sideboard/sideboard-server.cfg',
+            os.path.join(sideboard_dirs[1], 'test-defaults.ini'),
+            os.path.join(sideboard_dirs[1], 'test.ini')]
+        assert expected == get_config_files(
+            os.path.join(sideboard_dirs[0], 'config.py'), is_plugin=False)
+
+
+class TestSideboardGetConfigOverrides(object):
+    @pytest.fixture(params=[
+        (None, ['development-defaults.ini', 'development.ini']),
+        ('test-defaults.ini', ['test-defaults.ini', 'test.ini']),
+        ('test.ini;development.ini;test.ini', ['test.ini', 'development.ini']),
+        ('test-defaults.ini;test-defaults.ini', ['test-defaults.ini', 'test.ini']),
+        (' /absolute/path.ini ', ['/absolute/path.ini']),
+        ('/absolute/path.cfg', ['/absolute/path.cfg']),
+        (' relative/path.ini ', ['relative/path.ini']),
+        ('relative/path.cfg', ['relative/path.cfg']),
+        ('/absolute/path.cfg;relative/path.ini', ['/absolute/path.cfg', 'relative/path.ini']),
+        ('relative/path.cfg;/absolute/path.ini', ['relative/path.cfg', '/absolute/path.ini']),
+        ('  /absolute/path.cfg  ; relative/path.ini ', ['/absolute/path.cfg', 'relative/path.ini']),
+        (' /absolute/path-defaults.ini ', ['/absolute/path-defaults.ini', '/absolute/path.ini']),
+        ('/absolute/path-defaults.cfg', ['/absolute/path-defaults.cfg', '/absolute/path.cfg']),
+        (' relative/path-defaults.ini ', ['relative/path-defaults.ini', 'relative/path.ini']),
+        ('relative/path-defaults.cfg', ['relative/path-defaults.cfg', 'relative/path.cfg']),
+        ('/absolute/path-defaults.cfg;relative/path-defaults.ini', [
+            '/absolute/path-defaults.cfg',
+            '/absolute/path.cfg',
+            'relative/path-defaults.ini',
+            'relative/path.ini'
+        ])
+    ])
+    def config_overrides(self, request, monkeypatch):
+        if request.param[0] is None:
+            monkeypatch.delenv('SIDEBOARD_CONFIG_OVERRIDES', raising=False)
+        else:
+            monkeypatch.setenv('SIDEBOARD_CONFIG_OVERRIDES', request.param[0])
+        return request.param[1]
+
+    def test_get_config_overrides(self, config_overrides):
+        assert get_config_overrides() == config_overrides
+
+
+class TestSideboardGetConfigRoot(object):
     @pytest.fixture
     def dir_missing(self, monkeypatch):
         monkeypatch.setattr(os.path, 'isdir', Mock(return_value=False))
