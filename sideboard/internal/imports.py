6fc10db18eedcdeebd306b11bf94593d841f41b9
Rob Ruana <rob@robruana.com>
2017-04-22 18:24:55 -0400

    Preserves plugin import order using OrderedDict

--- sideboard/internal/imports.py
+++ sideboard/internal/imports.py
@@ -1,23 +1,32 @@
 from __future__ import unicode_literals
 import sys
 import importlib
+from collections import OrderedDict
 from glob import glob
+from itertools import chain
 from os.path import join, isdir, basename
 
 from sideboard.config import config
 
-plugins = {}
+
+plugins = OrderedDict()
+plugin_dirs = OrderedDict()
+
+
+def _discover_plugin_dirs():
+    unsorted_dirs = {basename(d).replace('-', '_'): d
+            for d in glob(join(config['plugins_dir'], '*'))
+            if isdir(d) and not basename(d).startswith('_')}
+
+    priority_plugins = config['priority_plugins']
+    nonpriority_plugins = sorted(set(unsorted_dirs.keys()).difference(priority_plugins))
+    sorted_plugins = chain(priority_plugins, nonpriority_plugins)
+
+    return [(name, unsorted_dirs[name]) for name in sorted_plugins]
 
 
 def _discover_plugins():
-    ordered = list(reversed(config['priority_plugins']))
-    plugin_dirs = [d for d in glob(join(config['plugins_dir'], '*')) if isdir(d) and not basename(d).startswith('_')]
-
-    # glob() results are not ordered, so we sort here to ensure non-prioritized plugins load in the same order
-    # regardless of OS-dependent arbitrary ordering
-    plugin_dirs = sorted(plugin_dirs, key=lambda d: basename(d))
-
-    for plugin_path in sorted(plugin_dirs, reverse=True, key=lambda d: (ordered.index(basename(d)) if basename(d) in ordered else -1)):
-        sys.path.append(plugin_path)
-        plugin_name = basename(plugin_path).replace('-', '_')
-        plugins[plugin_name] = importlib.import_module(plugin_name)
+    for name, path in _discover_plugin_dirs():
+        sys.path.append(path)
+        plugins[name] = importlib.import_module(name)
+        plugin_dirs[name] = path
