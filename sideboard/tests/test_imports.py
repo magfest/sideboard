258889f88f54d3f948c16fbde2a38d4fdc02a27c
Rob Ruana <rob@robruana.com>
2017-04-22 18:31:39 -0400

    Makes the test a little bit more robust

--- sideboard/tests/test_imports.py
+++ sideboard/tests/test_imports.py
@@ -15,12 +15,12 @@ class TestDiscoverPluginDirs:
         plugins_dir = tempfile.mkdtemp()
         try:
             monkeypatch.setitem(config, 'plugins_dir', plugins_dir)
-            plugin_names = ['_u', 'a', 'z', 'b', 'y', 'c', 'x']
+            plugin_names = ['_u', 'a-1', 'z-2', 'b_3', 'y_4', 'c-6', 'x_7']
             plugin_dirs = {name: join(plugins_dir, name) for name in plugin_names}
             for plugin_name, plugin_dir in plugin_dirs.items():
                 os.makedirs(plugin_dir)
             actual = _discover_plugin_dirs()
-            expected = [(name, plugin_dirs[name]) for name in sorted(plugin_names) if name != '_u']
+            expected = [(name.replace('-', '_'), plugin_dirs[name]) for name in sorted(plugin_names) if name != '_u']
             assert actual == expected
         finally:
             shutil.rmtree(plugins_dir, ignore_errors=True)
