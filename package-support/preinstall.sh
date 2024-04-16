6ae674ba8afd37d224ffa3cd6a23e1f81fea59b8
Mark Murnane <mark@hackafe.net>
2024-04-16 19:02:18 -0400

    Cleaning up old stuff

--- package-support/preinstall.sh
+++
@@ -1,5 +0,0 @@
-
-if ! id -u sideboard &>/dev/null; then
-    groupadd --force -r sideboard -g 600
-    useradd -r --shell /sbin/nologin  -uid 600 --gid sideboard sideboard
-fi
--- sideboard/lib/_threads.py
+++ sideboard/lib/_threads.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 import sys
 import time
 import heapq
-import ctypes
+import ctypes, ctypes.util
+import psutil
 import platform
 import traceback
 import threading
@@ -15,11 +16,15 @@ from six.moves.queue import Queue, Empty
 from sideboard.lib import log, config, on_startup, on_shutdown
 from sideboard.debugging import register_diagnostics_status_function
 
-try:
-    import prctl
-    import psutil
-except ImportError:
-    prctl = psutil = None  # For platforms without this support.
+# Replaces the prior prctl implementation with a direct call to pthread to change thread names
+libpthread_path = ctypes.util.find_library("pthread")
+pthread_setname_np = None
+if libpthread_path:
+    libpthread = ctypes.CDLL(libpthread_path)
+    if hasattr(libpthread, "pthread_setname_np"):
+        pthread_setname_np = libpthread.pthread_setname_np
+        pthread_setname_np.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+        pthread_setname_np.restype = ctypes.c_int
 
 
 def _get_linux_thread_tid():
@@ -43,18 +48,20 @@ def _get_linux_thread_tid():
 
 def _set_current_thread_ids_from(thread):
     # thread ID part 1: set externally visible thread name in /proc/[pid]/tasks/[tid]/comm to our internal name
-    if prctl and thread.name:
+    if pthread_setname_np and thread.name:
         # linux doesn't allow thread names > 15 chars, and we ideally want to see the end of the name.
         # attempt to shorten the name if we need to.
         shorter_name = thread.name if len(thread.name) < 15 else thread.name.replace('CP Server Thread', 'CPServ')
-        prctl.set_name(shorter_name)
+        if thread.ident is not None:
+            pthread_setname_np(thread.ident, shorter_name)
+
 
     # thread ID part 2: capture linux-specific thread ID (TID) and store it with this thread object
     # if TID can't be obtained or system call fails, tid will be -1
     thread.linux_tid = _get_linux_thread_tid()
 
 
-# inject our own code at the start of every thread's start() method which sets the thread name via prctl().
+# inject our own code at the start of every thread's start() method which sets the thread name via pthread().
 # Python thread names will now be shown in external system tools like 'top', '/proc', etc.
 def _thread_name_insert(self):
     _set_current_thread_ids_from(self)
