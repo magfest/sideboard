bcb20134575488f2fd88e7c156b4a3c89b01be1a
Mark Murnane <mark@hackafe.net>
2024-04-16 23:53:04 -0400

    Fixing thread info page

--- sideboard/lib/__init__.py
+++ sideboard/lib/__init__.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from sideboard.config import parse_config, config
 from sideboard.lib._utils import serializer, entry_point
 import sideboard.lib._redissession
+import sideboard.lib._threads
 
 __all__ = ['parse_config', 'config',
            'serializer', 'entry_point']
